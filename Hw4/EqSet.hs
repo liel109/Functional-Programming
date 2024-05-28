9bed78e3eac9ae1d3decdca7bc604afd65a6c92f
Yali Gal <yaligal1212@gmail.com>
2024-05-28 16:27:16 +0300

    Working EqSet

--- Hw4/EqSet.hs
+++ Hw4/EqSet.hs
@@ -18,12 +18,35 @@ import Data.Maybe
 newtype EqSet a = EqSet {getSet :: [a]}
 
 empty :: EqSet a
-member :: Eq a => a -> EqSet a -> Bool
-insert :: Eq a => a -> EqSet a -> EqSet a
-remove :: Eq a => a -> EqSet a -> EqSet a
-elems :: EqSet a -> [a]
+empty = EqSet []
 
-instance Eq a => Eq (EqSet a)
-instance Show a => Show (EqSet a)
+member :: Eq a => a -> EqSet a -> Bool
+member x (EqSet xs) = x `elem` xs
+
+insert :: Eq a => a -> EqSet a -> EqSet a
+insert x (EqSet xs) = if x `elem` xs then EqSet xs else EqSet (x:xs) 
+
+remove :: Eq a => a -> EqSet a -> EqSet a
+remove x (EqSet xs) = EqSet (delete x xs)
+
+elems :: EqSet a -> [a]
+elems = getSet 
+
+instance Eq a => Eq (EqSet a) where
+  (EqSet xs) == (EqSet ys) = all (\x -> x `elem` ys) xs && all (\y -> y `elem` xs) ys
+
+instance Show a => Show (EqSet a) where
+  show (EqSet xs) = "{" ++ show' xs ++ "}"
+    where
+      show' [] = ""
+      show' [y] = show y
+      show' (y:ys) = show y ++ ", " ++ show' ys
+
 instance Eq a => Semigroup (EqSet a)
+  where
+    (EqSet xs) <> (EqSet ys) = EqSet (xs `union` ys)
+
 instance Eq a => Monoid (EqSet a)
+  where
+    mempty = empty
+
