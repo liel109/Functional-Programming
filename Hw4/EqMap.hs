241bad40afc86cb6d051d70468d1aba2b6329dde
Yali Gal <yaligal1212@gmail.com>
2024-05-29 20:06:53 +0300

    Working EqMap

--- Hw4/EqMap.hs
+++ Hw4/EqMap.hs
@@ -4,7 +4,7 @@
 
 module EqMap (
   EqMap,
-  CombiningMap (..),
+  -- CombiningMap (..),
   empty,
   EqMap.insert, -- To avoid name clash with Data.List.insert
   member,
@@ -20,19 +20,64 @@ import Data.Semigroup (Arg (..))
 import EqSet (EqSet)
 import EqSet qualified
 
-data EqMap k v -- Complete me!
+data EqMap k v = EqMap {getMap :: EqSet (Arg k v)}
 
 empty :: EqMap k v
-member :: Eq k => k -> EqMap k v -> Bool
-insert :: Eq k => k -> v -> EqMap k v -> EqMap k v
-remove :: Eq k => k -> EqMap k v -> EqMap k v
-lookup :: Eq k => k -> EqMap k v -> Maybe v
-assocs :: EqMap k v -> [(k, v)]
+empty = EqMap EqSet.empty
 
-instance (Eq k, Eq v) => Eq (EqMap k v)
-instance (Show k, Show v) => Show (EqMap k v)
-instance Eq k => Semigroup (EqMap k v)
-instance Eq k => Monoid (EqMap k v)
+member :: Eq k => k -> EqMap k v -> Bool
+member key (EqMap m) = EqSet.member (Arg key undefined) m
+
+insert :: Eq k => k -> v -> EqMap k v -> EqMap k v
+insert key value m = EqMap $ EqSet.insert (Arg key value) (getMap (remove key m))
+
+remove :: Eq k => k -> EqMap k v -> EqMap k v
+remove key (EqMap m) = EqMap (EqSet.remove (Arg key undefined) m)
+
+lookup :: Eq k => k -> EqMap k v -> Maybe v
+lookup k (EqMap m) = go (EqSet.elems m)
+  where
+    go [] = Nothing
+    go (Arg k' v : xs)
+      | k == k' = Just v
+      | otherwise = go xs
+
+
+assocs :: EqMap k v -> [(k, v)]
+assocs (EqMap m) = go (EqSet.elems m)
+  where
+    go [] = []
+    go (Arg k v : xs) = (k, v) : go xs
+
+instance (Eq k, Eq v) => Eq (EqMap k v) where
+  m1 == m2 = getMap m1 == getMap m2
+
+instance (Show k, Show v) => Show (EqMap k v) where
+  show :: (Show k, Show v) => EqMap k v -> String
+  show (EqMap m) = "{" ++ show' (EqSet.elems m) ++ "}"
+    where
+      show' [] = ""
+      show' [Arg k v] = show k ++ " -> " ++ show v
+      show' (Arg k v:xs) = show k ++ " -> " ++ show v ++ ", " ++ show' xs
+
+instance Eq k => Semigroup (EqMap k v) where
+  m1 <> m2 = EqMap (getMap m2 <> getMap m1)
+
+instance Eq k => Monoid (EqMap k v) where
+    mempty = empty
+
 newtype CombiningMap k v = CombiningMap {getCombiningMap :: EqMap k v}
-instance (Eq k, Semigroup v) => Semigroup (CombiningMap k v)
-instance (Eq k, Semigroup v) => Monoid (CombiningMap k v)
+
+instance (Eq k, Semigroup v) => Semigroup (CombiningMap k v) where
+  cm1 <> cm2 =
+    CombiningMap $ EqMap $ foldr insertOrCombine (getMap $ getCombiningMap cm1) (EqSet.elems (getMap $ getCombiningMap cm2))
+    where
+      insertOrCombine (Arg k v) acc = getMap . insertWithCombine k v . EqMap $ acc
+      insertWithCombine k v m =
+        case EqMap.lookup k m of
+          Just v' -> EqMap.insert k (v' <> v) m
+          Nothing -> EqMap.insert k v m
+
+
+instance (Eq k, Semigroup v) => Monoid (CombiningMap k v) where
+  mempty = CombiningMap empty
