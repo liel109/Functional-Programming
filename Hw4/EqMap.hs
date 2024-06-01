f2e878e9a05fd1f9cd57e632380c5b5f88074885
Yali Gal <yaligal1212@gmail.com>
2024-06-01 13:04:48 +0300

    Part of section 3

--- Hw4/EqMap.hs
+++ Hw4/EqMap.hs
@@ -2,16 +2,17 @@
 {-# OPTIONS_GHC -Wall -Werror #-}
 {-# OPTIONS_GHC -Wno-unused-imports #-}
 
-module EqMap (
-  EqMap,
-  -- CombiningMap (..),
-  empty,
-  EqMap.insert, -- To avoid name clash with Data.List.insert
-  member,
-  remove,
-  EqMap.lookup, -- To avoid name clash with Prelude.lookup
-  assocs
-) where
+module EqMap
+  ( EqMap,
+    CombiningMap (..),
+    empty,
+    EqMap.insert, -- To avoid name clash with Data.List.insert
+    member,
+    remove,
+    EqMap.lookup, -- To avoid name clash with Prelude.lookup
+    assocs,
+  )
+where
 
 import Data.Either
 import Data.List
@@ -25,23 +26,22 @@ data EqMap k v = EqMap {getMap :: EqSet (Arg k v)}
 empty :: EqMap k v
 empty = EqMap EqSet.empty
 
-member :: Eq k => k -> EqMap k v -> Bool
+member :: (Eq k) => k -> EqMap k v -> Bool
 member key (EqMap m) = EqSet.member (Arg key undefined) m
 
-insert :: Eq k => k -> v -> EqMap k v -> EqMap k v
+insert :: (Eq k) => k -> v -> EqMap k v -> EqMap k v
 insert key value m = EqMap $ EqSet.insert (Arg key value) (getMap (remove key m))
 
-remove :: Eq k => k -> EqMap k v -> EqMap k v
+remove :: (Eq k) => k -> EqMap k v -> EqMap k v
 remove key (EqMap m) = EqMap (EqSet.remove (Arg key undefined) m)
 
-lookup :: Eq k => k -> EqMap k v -> Maybe v
+lookup :: (Eq k) => k -> EqMap k v -> Maybe v
 lookup k (EqMap m) = go (EqSet.elems m)
   where
     go [] = Nothing
     go (Arg k' v : xs)
       | k == k' = Just v
       | otherwise = go xs
-
 
 assocs :: EqMap k v -> [(k, v)]
 assocs (EqMap m) = go (EqSet.elems m)
@@ -58,13 +58,13 @@ instance (Show k, Show v) => Show (EqMap k v) where
     where
       show' [] = ""
       show' [Arg k v] = show k ++ " -> " ++ show v
-      show' (Arg k v:xs) = show k ++ " -> " ++ show v ++ ", " ++ show' xs
+      show' (Arg k v : xs) = show k ++ " -> " ++ show v ++ ", " ++ show' xs
 
-instance Eq k => Semigroup (EqMap k v) where
+instance (Eq k) => Semigroup (EqMap k v) where
   m1 <> m2 = EqMap (getMap m2 <> getMap m1)
 
-instance Eq k => Monoid (EqMap k v) where
-    mempty = empty
+instance (Eq k) => Monoid (EqMap k v) where
+  mempty = empty
 
 newtype CombiningMap k v = CombiningMap {getCombiningMap :: EqMap k v}
 
@@ -77,7 +77,6 @@ instance (Eq k, Semigroup v) => Semigroup (CombiningMap k v) where
         case EqMap.lookup k m of
           Just v' -> EqMap.insert k (v' <> v) m
           Nothing -> EqMap.insert k v m
-
 
 instance (Eq k, Semigroup v) => Monoid (CombiningMap k v) where
   mempty = CombiningMap empty
--- Hw4/HW4.hs
+++ Hw4/HW4.hs
@@ -11,6 +11,7 @@ module HW4 where
 
 import Data.Char (chr, ord)
 import Data.Either
+import Data.IntMap qualified as Eq
 import Data.List
 import Data.Maybe
 import Data.Semigroup (Arg (..))
@@ -26,73 +27,126 @@ class Serializable a where
 
 instance Serializable Int where
   serialize a = [a]
-  deserialize [a] = a 
+  deserialize [a] = a
+  deserialize _ = error "Invalid"
 
 instance Serializable Bool where
   serialize True = [1]
   serialize False = [0]
   deserialize [1] = True
   deserialize [0] = False
+  deserialize _ = error "Invalid"
 
 instance Serializable Char where
   serialize a = [ord a]
   deserialize [a] = chr a
+  deserialize _ = error "Invalid"
 
-instance Serializable a => Serializable (Maybe a) where
+instance (Serializable a) => Serializable (Maybe a) where
   serialize Nothing = [0]
   serialize (Just a) = 1 : serialize a
   deserialize [0] = Nothing
   deserialize (1 : a) = Just (deserialize a)
+  deserialize _ = error "Invalid"
 
-instance (Serializable a, Serializable b) => Serializable (a, b) 
-instance (Serializable a, Serializable b) => Serializable (Either a b)
-instance Serializable a => Serializable [a]
-instance (Serializable a, Eq a) => Serializable (EqSet a)
-instance (Serializable k, Eq k, Serializable v) => Serializable (EqMap k v)
+instance (Serializable a, Serializable b) => Serializable (a, b) where
+  serialize (a, b) = serialize a ++ serialize b
+  deserialize xs =
+    let (aSerialized, bSerialized) = splitAt (length (serialize (undefined :: a))) xs
+     in (deserialize aSerialized, deserialize bSerialized)
+
+instance (Serializable a, Serializable b) => Serializable (Either a b) where
+  serialize (Left a) = 0 : serialize a
+  serialize (Right b) = 1 : serialize b
+  deserialize (0 : a) = Left (deserialize a)
+  deserialize (1 : b) = Right (deserialize b)
+  deserialize _ = error "Invalid"
+
+instance (Serializable a) => Serializable [a] where
+  serialize [] = [0]
+  serialize (x : xs) = 1 : serialize x ++ serialize xs
+  deserialize [0] = []
+  deserialize (1 : x) =
+    let (a, b) = splitAt (length (serialize (undefined :: a))) x
+     in deserialize a : deserialize b
+  deserialize _ = error "Invalid"
+
+instance (Serializable a, Eq a) => Serializable (EqSet a) where
+  serialize = serialize . EqSet.elems
+  deserialize xs = foldr EqSet.insert EqSet.empty (deserialize xs :: [a])
+
+instance (Serializable k, Eq k, Serializable v) => Serializable (EqMap k v) where
+  serialize = serialize . EqMap.assocs
+  deserialize xs = foldr (uncurry EqMap.insert) EqMap.empty (deserialize xs :: [(k, v)])
 
 -- Section 3: Metric
 infinity :: Double
 infinity = 1 / 0
 
-class Eq a => Metric a where
+class (Eq a) => Metric a where
   distance :: a -> a -> Double
 
-instance Metric Double
-instance Metric Int
-instance Metric Char
+instance Metric Double where
+  distance x y = abs (x - y)
+
+instance Metric Int where
+  distance x y = abs (fromIntegral x - fromIntegral y)
+
+instance Metric Char where
+  distance x y = distance (ord x) (ord y)
 
 -- Euclidean distance
-instance (Metric a, Metric b) => Metric (a, b)
+instance (Metric a, Metric b) => Metric (a, b) where
+  distance (a1, b1) (a2, b2) = sqrt ((distance a1 a2) ** 2 + (distance b1 b2) ** 2)
 
-data ManhattanTuple a b = ManhattanTuple a b deriving Eq
-instance (Metric a, Metric b) => Metric (ManhattanTuple a b)
+data ManhattanTuple a b = ManhattanTuple a b deriving (Eq)
+
+instance (Metric a, Metric b) => Metric (ManhattanTuple a b) where
+  distance (ManhattanTuple a1 b1) (ManhattanTuple a2 b2) = distance a1 a2 + distance b1 b2
 
 -- Just and Nothing have distance of infinity.
 -- Two Justs measure the distance between the two values.
-instance Metric a => Metric (Maybe a)
+instance (Metric a) => Metric (Maybe a) where
+  distance (Just a) (Just b) = distance a b
+  distance _ _ = infinity
 
 -- Left and Right have a distance of infinity.
 -- Same constructores measure the distance between the two values.
-instance (Metric a, Metric b) => Metric (Either a b)
+instance (Metric a, Metric b) => Metric (Either a b) where
+  distance (Left a) (Left b) = distance a b
+  distance (Right a) (Right b) = distance a b
+  distance _ _ = infinity
 
 -- Lists of different sizes have distance of infinity.
 -- Euclidean distance.
-instance Metric a => Metric [a]
+instance (Metric a) => Metric [a] where
+  distance xs ys
+    | length xs /= length ys = infinity
+    | otherwise = sqrt (sum (zipWith (\x y -> distance x y ** 2) xs ys))
 
-newtype ManhattanList a = ManhattanList [a] deriving Eq
-instance Metric a => Metric (ManhattanList a)
+newtype ManhattanList a = ManhattanList [a] deriving (Eq)
+
+instance (Metric a) => Metric (ManhattanList a) where
+  distance (ManhattanList xs) (ManhattanList ys)
+    | length xs /= length ys = infinity
+    | otherwise = sum (zipWith distance xs ys)
 
 -- Returns the element with the shortest distance to the input.
 -- If there are no numbers whose distance is less than infinity, return Nothing.
-closest :: Metric a => a -> [a] -> Maybe a
--- Similar to the above, but uses a function move the element
--- to another metric space.
-closestOn :: Metric b => (a -> b) -> a -> [a] -> Maybe a
--- Will not swap elements whose distance is less than d, even if their
--- order implies they should be swapped.
-metricBubbleSort :: (Metric a, Ord a) => Double -> [a] -> [a]
--- Similar to the above, but uses a function to extract the value used for sorting.
-metricBubbleSortOn :: (Metric b, Ord b) => (a -> b) -> Double -> [a] -> [a]
+closest :: (Metric a) => a -> [a] -> Maybe a
+closest a xs = closest' a xs infinity a
 
--- Bonus (10 points).
-clusters :: Metric a => [a] -> [[a]]
+closest' :: (Metric a) => a -> [a] -> Double -> a -> Maybe a
+closest' a (x : xs) d c = if distance a x < d then closest' a xs (distance a x) x else closest' a xs d c
+closest' _ [] d c = if d == infinity then Nothing else Just c
+
+-- -- Similar to the above, but uses a function move the element
+-- -- to another metric space.
+-- closestOn :: (Metric b) => (a -> b) -> a -> [a] -> Maybe a
+-- -- Will not swap elements whose distance is less than d, even if their
+-- -- order implies they should be swapped.
+-- metricBubbleSort :: (Metric a, Ord a) => Double -> [a] -> [a]
+-- -- Similar to the above, but uses a function to extract the value used for sorting.
+-- metricBubbleSortOn :: (Metric b, Ord b) => (a -> b) -> Double -> [a] -> [a]
+-- -- Bonus (10 points).
+-- clusters :: (Metric a) => [a] -> [[a]]
